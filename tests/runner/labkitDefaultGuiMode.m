fbf3b3cdca1ca9579e9711ab9979e0cab01756b9
Ziyu Zhu <zhu_ziyu@outlook.com>
2026-07-23 13:20:36 -0500

    refactor: remove legacy test runner

--- tests/runner/labkitDefaultGuiMode.m
+++
@@ -1,8 +0,0 @@
-function mode = labkitDefaultGuiMode()
-%LABKITDEFAULTGUIMODE Return the default official GUI test visibility mode.
-
-    mode = string(getenv("LABKIT_GUI_TEST_MODE"));
-    if strlength(strtrim(mode)) == 0
-        mode = "hidden";
-    end
-end
